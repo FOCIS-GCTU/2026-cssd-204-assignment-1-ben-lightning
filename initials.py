fb532ec25fe5f010b853bd4b2d9c485b52465ffe
ben-lightning <2425403269@live.gctu.edu.gh>
2026-06-02 20:59:45 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,37 @@
+# File: BCL intialis
+# Description: Prints the intials BCL in a stylized layout.
+# Assignment Number: 2
+#
+# Name: Benedict Coffie-Long
+# STUDENT ID: 2425403269
+# Email: coffiebenedict94@gmail.com
+# Grader: Benedict 
+#
+# On my honor, Benedict Coffie-Long, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # This main function executes the stylized layout printing for the initials BCL.
+    
+    print()
 
+    print("...BCL")
+
+    print()
+
+    print("...BBBBBBBBBB.......... CCCCCCCCC..........LL...............")
+    print("...BB........B.........CC........CC........LL...............")
+    print("...BB........B.........CC..................LL...............")
+    print("...BBBBBBBBBB..........CC..................LL...............")
+    print("...BBBBBBBBBB..........CC..................LL...............")
+    print("...BB........B.........CC..................LL...............")
+    print("...BB........B.........CC..................LL...............")
+    print("...BB........B.........CC..................LL...............")
+    print("...BB........B....**...CC........CC...**...LL.............**")
+    print("...BBBBBBBBBB.....**....CCCCCCCCC.....**...LLLLLLLLLLLL...**")
+
+    print()
 
 
 main()
